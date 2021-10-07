eab017e6fca55a836522cce994a0e90d4aba4640
devulogi <devulogijrp.github@gmail.com>
2021-10-07 10:35:27 +0800

    added enqueue method and tested: pass

--- test/Queue.spec.js
+++ test/Queue.spec.js
@@ -14,6 +14,19 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 // ------------------------------------------------------- //
@@ -25,8 +38,14 @@ beforeEach(() => {
 });
 
 describe('Queue', () => {
-  it('should create a new queue instance', done => {
+  xit('should create a new queue instance', done => {
     expect(queue.last.value).to.equal(8);
+    done();
+  });
+
+  xit('should add a new node to the end', done => {
+    expect(queue.enqueue(9).last.value).to.equal(9);
+    expect(queue.length).to.equal(2);
     done();
   });
 });
