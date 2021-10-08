2c90df9bc8eb8a0434b157f74e3649ffc8dc2bfe
devulogi <devulogijrp.github@gmail.com>
2021-10-08 21:00:08 +0800

    created bst

--- test/Tree.spec.js
+++ test/Tree.spec.js
@@ -12,6 +12,37 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    // create a new node
+    const newNode = new Node(value);
+    // if root is null, assign new node to root
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    // track current node value
+    let temp = this.root;
+    while (true) {
+      // return undefined, if current node and new node's value is the same
+      if (temp.value === newNode.value) return undefined;
+      // compare value's of current node and new node to decide which branch to go (L or R)
+      if (temp.value > newNode.value) {
+        // if current  node branch is "null", assign new node
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
 
 // ------------------------------------------------------- //
