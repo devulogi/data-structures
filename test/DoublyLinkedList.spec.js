85a9d261c3e43265886f498622ed706eaea05a09
devulogi <devulogijrp.github@gmail.com>
2021-10-10 15:42:02 +0800

    unshift method added and tested: passed

--- test/DoublyLinkedList.spec.js
+++ test/DoublyLinkedList.spec.js
@@ -41,6 +41,21 @@ class DoublyLinkedList {
     this.length--;
     return this;
   }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const temp = this.head;
+      newNode.next = temp;
+      temp.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 // ------------------------------------------------------- //
@@ -67,6 +82,12 @@ describe('DoublyLinkedList', () => {
     newDoublyLinkedList.push(9);
     expect(newDoublyLinkedList.pop().tail.value).to.equal(8);
     expect(newDoublyLinkedList.length).to.equal(1);
+    done();
+  });
+
+  xit('should add a new value at the beginning', done => {
+    expect(newDoublyLinkedList.unshift(7).head.value).to.equal(7);
+    expect(newDoublyLinkedList.length).to.equal(2);
     done();
   });
 });
