f64cbbdab5e4cf64d38cf74623a78de0fc5f2edf
devulogi <devulogijrp.github@gmail.com>
2021-10-12 23:15:52 +0800

    added addEdge method and tested: passed

--- test/Graph.spec.js
+++ test/Graph.spec.js
@@ -12,19 +12,39 @@ class Graph {
     }
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+    return false;
+  }
 }
+
+let graph = null;
+
+beforeEach(() => {
+  graph = new Graph();
+});
 
 describe('Graph', () => {
   xit('should create instance of Graph', done => {
-    const graph = new Graph();
     expect(graph).to.haveOwnProperty('adjacencyList');
     done();
   });
 
   xit('should create vertex in adjacencyList', done => {
-    const graph = new Graph();
     graph.addVertex('A');
     expect(graph.adjacencyList).to.haveOwnProperty('A');
+    done();
+  });
+
+  xit('should insert vertices and connect them together', done => {
+    graph.addVertex('1');
+    graph.addVertex('2');
+    expect(graph.addEdge('1', '2')).to.be.true;
     done();
   });
 });
